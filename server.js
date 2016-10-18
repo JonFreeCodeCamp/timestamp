0f703143172c5658fab1a7a67e616e1d220aa7ab
JonFreeCodeCamp <JonFreeCodeCamp@users.noreply.github.com>
2016-10-18 04:35:24 +0000

    implement time server

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 var express = require('express');
 
+var app = express();
+
+app.get('/', (req, res) => {
+    res.end("Add something to the query ie. http://thissite/somequery");
+});
+
+app.get('/:query', (req, res) => {
+    var d = null;
+    if (!Number.isNaN(parseInt(req.params.query, 10))) {
+        d = new Date(parseInt(req.params.query, 10));
+    } else {
+        d = new Date(req.params.query);
+        if (d.toDateString() === "Invalid Date") d = null;
+    }
+    var result = {"unix": null, "natural": null};
+    if (d) {
+        result["unix"] = d.getTime();
+        result["natural"] = d.toDateString();
+    }
+    res.json(result);
+});
+
+app.listen(8080);
